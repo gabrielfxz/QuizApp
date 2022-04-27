53237ce78a1ed2cb0075d5fa445f85bfc4c1b43a
Gabriel Gonçalves Perozim <gabrielperozim7@gmail.com>
2022-04-27 11:10:32 -0300

    Loading questions directly from an online API now
    - changed the path we get our questions from again, this time, from an online API called Open Trivia DB.
    - created a spinning loader which appears while the game is being loaded.
    - removed questions.json as we don't need it anymore

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -12,12 +14,31 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch('questions.json')
+fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
     .then(res => {
         return res.json();
     })
     .then(loadedQuestions => {
-        questions = loadedQuestions;
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+
         startGame();
     })
     .catch(err => {
@@ -34,6 +55,8 @@ startGame = () => {
     availableQuestions = [...questions]; // full copy
     console.log(availableQuestions);
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
@@ -47,7 +70,7 @@ getNewQuestion = () => {
     progressText.innerText = `Question ${questionCounter} / ${MAX_QUESTIONS}`;
     //update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-    
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
